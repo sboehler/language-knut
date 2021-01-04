b1f7c33a0b818e9604f66f79e650450d93cd8629
Silvio Böhler <silvio@monadic.ch>
2021-01-04 01:01:24 +0100

    Fix linting errors

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,11 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    const disposable = vscode.languages.registerFoldingRangeProvider("knut", new KnutFoldingProvider())
+    const disposable = vscode.languages.registerFoldingRangeProvider("knut", new KnutFoldingProvider());
     context.subscriptions.push(disposable);
 
-    const disp = vscode.languages.registerDocumentSymbolProvider("knut", new KnutOutlineProvider())
-    context.subscriptions.push(disp)
+    const disp = vscode.languages.registerDocumentSymbolProvider("knut", new KnutOutlineProvider());
+    context.subscriptions.push(disp);
 }
 
 // this method is called when your extension is deactivated
@@ -15,37 +15,37 @@ export function deactivate() { }
 
 class KnutFoldingProvider implements vscode.FoldingRangeProvider {
     provideFoldingRanges(document: vscode.TextDocument, context: vscode.FoldingContext, token: vscode.CancellationToken): vscode.ProviderResult<vscode.FoldingRange[]> {
-        const result: vscode.FoldingRange[] = []
-        const stack: (vscode.FoldingRange | undefined)[] = []
+        const result: vscode.FoldingRange[] = [];
+        const stack: (vscode.FoldingRange | undefined)[] = [];
         for (let l = 0; l < document.lineCount; l++) {
-            const line = document.lineAt(l)
-            let stars = 0
+            const line = document.lineAt(l);
+            let stars = 0;
             while (line.text[stars] === '*') {
-                stars++
+                stars++;
             }
             if (stars === 0) {
-                continue
+                continue;
             }
             while (stack.length > stars - 1) {
-                const r = stack.pop()
+                const r = stack.pop();
                 if (r) {
-                    r.end = l - 1
-                    result.push(r)
+                    r.end = l - 1;
+                    result.push(r);
                 }
             }
             while (stack.length < stars - 1) {
-                stack.push(new vscode.FoldingRange(-1, -1))
+                stack.push(new vscode.FoldingRange(-1, -1));
             }
-            stack.push(new vscode.FoldingRange(l, l))
+            stack.push(new vscode.FoldingRange(l, l));
         }
         while (stack.length > 0) {
-            const r = stack.pop()
+            const r = stack.pop();
             if (r) {
-                r.end = document.lineCount - 1
-                result.push(r)
+                r.end = document.lineCount - 1;
+                result.push(r);
             }
         }
-        return result
+        return result;
     }
 
 }
@@ -54,41 +54,41 @@ class KnutFoldingProvider implements vscode.FoldingRangeProvider {
 class KnutOutlineProvider implements vscode.DocumentSymbolProvider {
 
     provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.SymbolInformation[] | vscode.DocumentSymbol[]> {
-        const result: vscode.DocumentSymbol[] = []
-        const stack: (vscode.DocumentSymbol | undefined)[] = []
+        const result: vscode.DocumentSymbol[] = [];
+        const stack: (vscode.DocumentSymbol | undefined)[] = [];
         for (let l = 0; l < document.lineCount; l++) {
-            const line = document.lineAt(l)
-            let stars = 0
+            const line = document.lineAt(l);
+            let stars = 0;
             while (line.text[stars] === '*') {
-                stars++
+                stars++;
             }
             if (stars === 0) {
-                continue
+                continue;
             }
             while (stack.length > stars - 1) {
-                const r = stack.pop()
+                const r = stack.pop();
                 if (r) {
-                    r.range = new vscode.Range(r.range.start, document.lineAt(l - 1).range.end)
+                    r.range = new vscode.Range(r.range.start, document.lineAt(l - 1).range.end);
                 }
             }
             while (stack.length < stars - 1) {
-                stack.push(undefined)
+                stack.push(undefined);
             }
-            const s = new vscode.DocumentSymbol(line.text.substr(stars).trim(), "", vscode.SymbolKind.Function, line.range, line.range)
+            const s = new vscode.DocumentSymbol(line.text.substr(stars).trim(), "", vscode.SymbolKind.Function, line.range, line.range);
             if (stack.length > 0) {
-                const parent = stack[stack.length - 1]
-                parent?.children.push(s)
+                const parent = stack[stack.length - 1];
+                parent?.children.push(s);
             } else {
-                result.push(s)
+                result.push(s);
             }
-            stack.push(s)
+            stack.push(s);
         }
         while (stack.length > 0) {
-            const r = stack.pop()
+            const r = stack.pop();
             if (r) {
-                r.range = new vscode.Range(r.range.start, document.lineAt(document.lineCount - 1).range.end)
+                r.range = new vscode.Range(r.range.start, document.lineAt(document.lineCount - 1).range.end);
             }
         }
-        return result
+        return result;
     }
 }
